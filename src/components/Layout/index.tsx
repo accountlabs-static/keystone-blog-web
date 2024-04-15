bb06617fb503adb20b05db44110141c3bdbafd87
Cao Xi <1060010408@qq.com>
2024-04-15 17:15:44 +0800

    feat: add gtag

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { ReactNode } from 'react';
 import {Header, Footer} from 'header-footer';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 interface LayoutProps {
   children: ReactNode
@@ -8,10 +9,11 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <>
+    <html lang='en'>
+      <GoogleTagManager gtmId="G-WCBLWW84S2" />
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </html>
   )
 }
