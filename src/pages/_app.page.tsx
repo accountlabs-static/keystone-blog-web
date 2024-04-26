f25331ff9cdd6c86d9c012c57d5abe22107bb44c
rea <hi@rea.ink>
2024-04-26 10:28:30 +0800

    fix: dynamic import header footer

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,16 @@
-import type { AppProps } from 'next/app';
-import React from 'react';
-import Layout from '../components/Layout';
-import '../styles/global.css';
-import '../styles/fonts.css';
-import { mont_serrat, open_sans } from '../styles/font';
-import 'header-footer/dist/style.css';
+import type { AppProps } from 'next/app'
+import React from 'react'
+import Layout from '../components/Layout'
+import '../styles/global.css'
+import '../styles/fonts.css'
+import { mont_serrat, open_sans } from '../styles/font'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className={`${open_sans.variable} ${mont_serrat.variable}`}>
-    <Layout><Component {...pageProps} /></Layout>
-  </div>
+  return (
+    <div className={`${open_sans.variable} ${mont_serrat.variable}`}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
+  )
 }
