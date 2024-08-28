dcaa6104d765ad5dc34d38c67a6e5d026526f780
Rea <hi@rea.ink>
2024-08-28 11:03:04 +0800

    feat: add ga (#18)
    feat: add ga

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,17 +1,21 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
+import 'gardevoir'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import Layout from '../components/Layout'
 import '../styles/global.css'
 import '../styles/fonts.css'
-import 'gardevoir'
 import { mont_serrat, open_sans } from '../styles/font'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${open_sans.variable} ${mont_serrat.variable}`}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
+    <>
+      <div className={`${open_sans.variable} ${mont_serrat.variable}`}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
+      <GoogleAnalytics gaId="G-WCBLWW84S2" />
+    </>
   )
 }
