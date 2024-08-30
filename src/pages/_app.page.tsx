c4dd563e36ff0176bfe93ecc1408334e4a693ede
rea <hi@rea.ink>
2024-08-30 09:54:54 +0800

    feat: add search console tag

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -6,10 +6,17 @@ import Layout from '../components/Layout'
 import '../styles/global.css'
 import '../styles/fonts.css'
 import { mont_serrat, open_sans } from '../styles/font'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="nwIg5t9B3A_0g81tXoaDStD6Z2uYyCDU7GBSALU7ZDM"
+        />
+      </Head>
       <div className={`${open_sans.variable} ${mont_serrat.variable}`}>
         <Layout>
           <Component {...pageProps} />
